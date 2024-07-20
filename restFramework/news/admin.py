e7a3eb0451a9a16a95bf3b0899079859aea54968
Yusuf B <yusufkygsz.b@gmail.com>
2024-07-20 09:37:04 +0300

    article model created and added to admin page

--- restFramework/news/admin.py
+++ restFramework/news/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 from news.models import Makale
 
 # Register your models here.
-
-
-# First Step includes setting and some arrangment relating to library for this project
+admin.site.register(Makale)
