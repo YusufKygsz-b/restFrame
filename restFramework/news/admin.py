bb87cbf3c37fdf3a2234da7e95f4521695adf365
Yusuf B <yusufkygsz.b@gmail.com>
2024-07-20 09:33:21 +0300

    First Step includes setting and some arrangment relating to library for this project

---
+++ restFramework/news/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from news.models import Makale
+
+# Register your models here.
+
+
+# First Step includes setting and some arrangment relating to library for this project
