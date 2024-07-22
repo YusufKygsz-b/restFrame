f211805834e1fecafde86635b9eb01079513974e
Yusuf B <yusufkygsz.b@gmail.com>
2024-07-22 09:09:44 +0300

    api_view created and applied

--- restFramework/news/api/views.py
+++ restFramework/news/api/views.py
@@ -12,13 +12,21 @@ from news.api.serializers import MakaleSerializer
 # Bu yapılar sayesinde, sunucumuza gelen istekleri, ya da gönderilen
 # responsları kontrol etmek için gerekli tüm kodlar hazır gelecektir.
 
-
 # api view içerisinde bir liste içerisinde kullanılacak olan metod
 # yazılmalı
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def makale_list_create_api_view(request):
     
     if request.method == 'GET':
         makaleler = Makale.objects.filter(aktif=True) # burada nesnelerden oluşan bir query set bulunmakta
-        serializer = MakaleSerializer(makaleler) # query set !!!
+        serializer = MakaleSerializer(makaleler, many = True) # query set !!!
         return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = MakaleSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        Response(status=status.HTTP_400_BAD_REQUEST)
