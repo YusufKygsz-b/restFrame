2288c8b79c7ef619af2f84324f82020f075eb26b
Yusuf B <yusufkygsz.b@gmail.com>
2024-07-23 12:16:44 +0300

    RetrieveUpdateDestroyAPIView,  ListModelMixin, CreateModelMixin ,GenericAPIView practice

--- restFramework/news/api/views.py
+++ restFramework/news/api/views.py
@@ -7,56 +7,104 @@ from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
 
 
-class GazeteciListCreateAPIView(APIView):
-    def get(self, request):
-        yazarlar = Gazeteci.objects.all()
-        serializer = GazeteciSerializer(yazarlar, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = GazeteciSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 
 
-class MakeListCreateAPIView(APIView):
-    def get(self, request):
-        makaleler = Makale.objects.filter(aktif=True)
-        serializer = MakaleSerializer(makaleler, many=True, context={'request': request})
-        return Response(serializer.data)
+class GazeteciListCreateAPIView(ListModelMixin, CreateModelMixin ,GenericAPIView):
 
-    def post(self, request):
-        serializer = MakaleSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Gazeteci.objects.all()
+    serializer_class = GazeteciSerializer
 
-class MakaleDetailAPIView(APIView):
-    def get_object(self, pk):
-        makale_instance = get_object_or_404(Makale, pk=pk)
-        return makale_instance
+    # Listelemek için Get metodu
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+        
+    # Create a model
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-    def get(self, request, pk):
-        makale = self.get_object(pk=pk)
-        serializer = MakaleSerializer(makale, context={'request': request})
-        return Response(serializer.data)
 
-    def put(self, request, pk):
-        makale = self.get_object(pk=pk)
-        serializer = MakaleSerializer(makale, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class MakeListCreateAPIView(ListCreateAPIView):
+    queryset = Makale.objects.filter(aktif=True)
+    serializer_class = MakaleSerializer
 
-    def delete(self, request, pk):
-        makale = self.get_object(pk=pk)
-        makale.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
+"""
+class MakaleDetailAPIView(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView ):
+    
+    queryset = Makale.objects.all()
+    serializer_class = MakaleSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+"""
+
+class MakaleDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Makale.objects.all()
+    serializer_class = MakaleSerializer
+
+"""
+# class MakeListCreateAPIView(APIView):
+#     def get(self, request):
+#         makaleler = Makale.objects.filter(aktif=True)
+#         serializer = MakaleSerializer(makaleler, many=True, context={'request': request})
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = MakaleSerializer(data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+# class GazeteciListCreateAPIView(APIView):
+#     def get(self, request):
+#         yazarlar = Gazeteci.objects.all()
+#         serializer = GazeteciSerializer(yazarlar, many=True, context={'request': request})
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = GazeteciSerializer(data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""    
+
+
+
+# class MakaleDetailAPIView(APIView):
+#     def get_object(self, pk):
+#         makale_instance = get_object_or_404(Makale, pk=pk)
+#         return makale_instance
+
+#     def get(self, request, pk):
+#         makale = self.get_object(pk=pk)
+#         serializer = MakaleSerializer(makale, context={'request': request})
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         makale = self.get_object(pk=pk)
+#         serializer = MakaleSerializer(makale, data=request.data, context={'request': request})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk):
+#         makale = self.get_object(pk=pk)
+#         makale.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 """
 #api_view decorator (function based view yaratabilmek için)
